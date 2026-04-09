1065744ae3f616ca9d185c3019299e760603d14d
mostafamohamed-code <mostafamohamed3m10@gmail.com>
2026-04-09 11:51:18 +0200

    Task Completed

---
+++ E-Commerce-System/E-Commerce-System/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_System
+{
+
+	public class EmailSender : INotificationSender
+	{
+		public void Send(string message)
+		{
+			Console.WriteLine($"[Email Sent]: {message}");
+		}
+	}
+
+	public class SMSSender : INotificationSender
+	{
+		public void Send(string message)
+		{
+			Console.WriteLine($"[SMS Sent]: {message}");
+		}
+	}
+
+}
