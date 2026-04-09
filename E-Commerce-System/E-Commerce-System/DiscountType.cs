7436232bfafe5916be9cdc5cf4efe8a42015a889
EslamAraby <ea7724373@gmail.com>
2026-04-09 11:46:45 +0200

    discount

---
+++ E-Commerce-System/E-Commerce-System/DiscountType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_System
+{
+    public class FixedDiscount : IDiscountStrategy
+    {
+        public double ApplyDiscount(double total){ return total - 50; }
+    }
+
+    public class PercentageDiscount : IDiscountStrategy
+    {
+        public double ApplyDiscount(double total) { return total * 0.9; }
+    }
+}
